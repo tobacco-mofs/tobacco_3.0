added some more scaling options, can now fix any unit cell parameter and have different options for pre-scaling

scale.py
@@ -30,15 +30,20 @@ def objective(V, ncra, ncca, Alpha, ne, nv, Bstar_inv, SBU_IP):
 
 	return O1
 
-def scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,Bstar,alpha,num_edges,NET_2D,PATIENCE,SCALING_ITERATIONS):
+def scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,Bstar,alpha,num_edges,FIX_UC,PATIENCE,SCALING_ITERATIONS,PRE_SCALE,SCALING_CONVERGENCE_TOLERANCE):
 
+	scale_tol = SCALING_CONVERGENCE_TOLERANCE
 	max_length = 0
 	for line in all_SBU_coords:
 		for length in [np.linalg.norm(s[1]) for s in line[1]]:
 			if length > max_length:
 				max_length = length
 
-	scale_guess = (max_length / max_le) * 2.0
+	if PRE_SCALE == 'none':
+		scale_guess = 1.0
+	else:
+		scale_guess = (max_length / max_le) * PRE_SCALE
+
 	all_SBU_ip = []
 	all_SBU_ip_append = all_SBU_ip.append
 	for sbu in all_SBU_coords:
@@ -65,10 +70,19 @@ def scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,
 	
 	init_variables = [scale_guess * a,scale_guess * b, scale_guess * c,ang_alpha,ang_beta,ang_gamma] + covars_values
 
-	if NET_2D:
-		uc_bounds = ((0,None),(0,None),(c,c),(20,160),(20,160),(20,160))
+	if np.any(FIX_UC):
+
+		uc_bounds = []
+		uc_bounds_append = uc_bounds.append
+		for f,p in zip(FIX_UC, init_variables[0:6]):
+			if f:
+				uc_bounds_append((p,p))
+			else:
+				uc_bounds_append((0,None))
+		uc_bounds = tuple(uc_bounds)
 	else:
 		uc_bounds = ((0,None),(0,None),(0,None),(20,160),(20,160),(20,160))
+
 	x_bounds = tuple([(None,None) for x in covars_values])
 	bounds = uc_bounds + x_bounds
 
@@ -108,7 +122,7 @@ def scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,
 			res = minimize(objective, init_variables, args=(ncra,ncca,alpha,num_edges,num_vertices,Bstar_inv,all_SBU_ip),
 							method='L-BFGS-B',
 							bounds=bounds,
-							options={'disp':False, 'gtol':1E-12, 'ftol':1E-12},
+							options={'disp':False, 'gtol':scale_tol, 'ftol':scale_tol},
 							callback=callbackF)
 
 			uc_params = res.x[0:6]

tobacco.py
@@ -52,7 +52,9 @@ ORIENTATION_DEPENDENT_NODES = configuration.ORIENTATION_DEPENDENT_NODES # added 
 
 PLACE_EDGES_BETWEEN_CONNECTION_POINTS = configuration.PLACE_EDGES_BETWEEN_CONNECTION_POINTS
 RECORD_CALLBACK = configuration.RECORD_CALLBACK
-NET_2D = configuration.NET_2D
+FIX_UC = configuration.FIX_UC
+PRE_SCALE = configuration.PRE_SCALE
+SCALING_CONVERGENCE_TOLERANCE = configuration.SCALING_CONVERGENCE_TOLERANCE
 ####### Global options #######
 
 pi = np.pi
@@ -217,8 +219,7 @@ for template in os.listdir('templates'):
 	
 			ea_dict = assign_node_vecs2edges(TG, unit_cell, SYMMETRY_TOL)
 			all_SBU_coords = SBU_coords(TG, ea_dict, CONNECTION_SITE_BOND_LENGTH)
-	
-			sc_a,sc_b,sc_c,sc_alpha,sc_beta,sc_gamma,sc_covar,Bstar_inv,max_length,callbackresults,ncra,ncca = scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,Bstar,alpha,num_edges,NET_2D,YOU_ARE_PATIENT,SCALING_ITERATIONS)
+			sc_a,sc_b,sc_c,sc_alpha,sc_beta,sc_gamma,sc_covar,Bstar_inv,max_length,callbackresults,ncra,ncca = scale(all_SBU_coords,a,b,c,ang_alpha,ang_beta,ang_gamma,max_le,num_vertices,Bstar,alpha,num_edges,FIX_UC,YOU_ARE_PATIENT,SCALING_ITERATIONS,PRE_SCALE,SCALING_CONVERGENCE_TOLERANCE)
 	
 			print '*******************************************'
 			print '   The scaled unit cell parameters are : ' 
@@ -231,7 +232,19 @@ for template in os.listdir('templates'):
 			print 'beta : ', sc_beta
 			print 'gamma : ', sc_gamma
 			print ''
-	
+
+			for sc, name in zip((sc_a, sc_b, sc_c), ('a', 'b', 'c')):
+				cflag = False
+				if sc < 1.0:
+					print 'unit cell parameter', name, 'has collapsed during scaling!'
+					print 'try re-running with', name, 'fixed, with a larger value for PRE_SCALE, or with a higher SCALING_CONVERGENCE_TOLERANCE'
+					print 'no cif will be written'
+					cflag = True
+			print ''
+
+			if cflag:
+				continue
+
 			scaled_params = [sc_a,sc_b,sc_c,sc_alpha,sc_beta,sc_gamma]
 		
 			sc_Alpha = np.r_[alpha[0:num_edges-num_vertices+1,:], sc_covar]